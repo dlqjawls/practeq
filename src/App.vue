0ecc225b486ce18000d43a2c2d01ab42140b8c22
dlqjawls <qjawls2617@naver.com>
2024-11-06 17:38:40 +0900

    1106

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <!-- FadeLogo 컴포넌트를 제외한 불필요한 부분 삭제 -->
+    <FadeLogo logoSrc="src/assets/입장.gif" :displayDuration="5000" />
+  </div>
+</template>
+
+<script>
+import FadeLogo from './components/FadeLogo.vue';
+
+export default {
+  components: {
+    FadeLogo,
+  },
+};
+</script>
+
+<style>
+/* 불필요한 스타일이 있으면 삭제 */
+</style>
---
+++ src/components/FadeLogo.vue
@@ -0,0 +1,54 @@
+<template>
+  <div v-if="showLogo" class="logo-container">
+    <transition name="fade" @after-leave="showLogo = false">
+      <img :src="logoSrc" alt="Logo" class="logo" />
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FadeLogo",
+  props: {
+    logoSrc: {
+      type: String,
+      required: true, // 로고 이미지 경로를 필수로 전달받음
+    },
+    displayDuration: {
+      type: Number,
+      default: 5000, // 기본값 5초
+    },
+  },
+  data() {
+    return {
+      showLogo: true,
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showLogo = false;
+    }, this.displayDuration);
+  },
+};
+</script>
+
+<style scoped>
+.logo-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  overflow: hidden;
+}
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 1s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+.logo {
+  width: 100vw;
+  height: 100vh;
+  object-fit: cover;
+}
+</style>
